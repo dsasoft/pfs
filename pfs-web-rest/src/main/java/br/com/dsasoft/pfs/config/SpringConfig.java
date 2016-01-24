1eb4ce0521022c186006a0da3ebbbbf8ee9453ed
danilo <danilo@danilo-laptop>
2016-01-24 20:31:58 -0200

    [poc/rest-springmvc] Initial setup

---
+++ pfs-web-rest/src/main/java/br/com/dsasoft/pfs/config/SpringConfig.java
@@ -0,0 +1,30 @@
+package br.com.dsasoft.pfs.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"br.com.dsasoft.pfs.resources"})
+public class SpringConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+	@Bean
+	public InternalResourceViewResolver viewResolver(){
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+}
---
+++ pfs-web-rest/src/main/java/br/com/dsasoft/pfs/config/SpringInitializer.java
@@ -0,0 +1,29 @@
+package br.com.dsasoft.pfs.config;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { SpringConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[]{"/"};
+	}
+	
+	@Override
+	protected Filter[] getServletFilters(){
+		return new Filter[]{ new CORSFilter()};
+	}
+
+}
