f20ac4754bc9eea5024143f08014f4a815ec0e5d
Abreu <e-doau@gft.com>
2016-02-08 08:27:45 -0200

    [pfs-web-rest][pfs-rest-service]

---
+++ pfs-rest-service/src/main/java/br/com/dsasoft/pfs/resources/ApiDocumantation.java
@@ -0,0 +1,32 @@
+package br.com.dsasoft.pfs.resources;
+
+//@Configuration
+//@EnableSwagger2
+public class ApiDocumantation {
+
+//	@Autowired
+//	private TypeResolver typeResolver;
+//	@Bean
+//    public Docket documentation() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.any())
+//                .paths(PathSelectors.regex("/api/.*"))
+//                .build()
+//                .apiInfo(metadata());
+//    }
+//
+//    @Bean
+//    public UiConfiguration uiConfig() {
+//        return UiConfiguration.DEFAULT;
+//    }
+//
+//    private ApiInfo metadata() {
+//        return new ApiInfoBuilder()
+//                .title("Title")
+//                .description("Description")
+//                .version("1.0")
+//                .contact("email@dsasoft.com.br")
+//                .build();
+//    }
+}
