b257b9d169f61ff92dfe34ddb6e2e2ceb97c34b3
Danilo de Sousa Abreu <danilo-abreu@Danilos-MacBook-Pro.local>
2016-03-20 21:52:00 -0300

    [pfs-web-rest]

--- pfs-web-rest/src/main/java/br/com/dsasoft/pfs/resources/AccountResources.java
+++ pfs-web-rest/src/main/java/br/com/dsasoft/pfs/resources/AccountResources.java
@@ -2,11 +2,14 @@ package br.com.dsasoft.pfs.resources;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.dsasoft.pfs.entity.AccountEntity;
 import br.com.dsasoft.pfs.facade.AccountFacade;
@@ -24,18 +27,20 @@ public class AccountResources {
 	}
 	
 	@RequestMapping( value = "/all", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE )
-	public List<AccountEntity> listAccount(){
-		return facade.listAll();
+	@ResponseBody
+	public ResponseEntity<List<AccountEntity>> listAccount(){
+		return new ResponseEntity<List<AccountEntity>>(facade.listAll(), HttpStatus.OK);
 	}
 	
 	@RequestMapping( value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public Account getAccount(@PathVariable("id") Long id){
-		return facade.findById(id);
+	public ResponseEntity<Account> getAccount(@PathVariable("id") Long id){
+		return new ResponseEntity<Account>(facade.findById(id), HttpStatus.OK);
 	}
 	
 	@RequestMapping( value = "/all/except/{id}", method = RequestMethod.GET, produces  = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public List<AccountEntity> listAccount(@PathVariable("id") Long id){
+	@ResponseBody
+	public ResponseEntity<List<AccountEntity>> listAccount(@PathVariable("id") Long id){
 		AccountFacade facadeAccount = new AccountFacade(null);
-		return facadeAccount.listAllExceptId(id);
+		return new ResponseEntity<List<AccountEntity>>(facadeAccount.listAllExceptId(id), HttpStatus.OK);
 	}
 }
